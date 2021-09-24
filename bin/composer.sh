7378afe8abe2879709a71ca8332505dbb6947836
Jean-Charles van der Bruggen <jeanch@8trust.com>
2021-09-24 11:46:59 +0200

    Update composer.sh

--- bin/composer.sh
+++ bin/composer.sh
@@ -6,4 +6,3 @@ docker run --rm --interactive --tty \
   --volume ${COMPOSER_HOME:-$HOME/.composer}:/tmp/.composer \
   --user $(id -u):$(id -g) \
   composer:1.5.0 ${@:1}
-  #faire un pull request car c'est la bonne version de composer, celle qui reprend la bonne version de php
